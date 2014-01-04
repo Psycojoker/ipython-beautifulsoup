f435cf012cb641712565049fffa2603685fc7168
Astalaseven <gatien.bovyn@gmail.com>
2014-01-04 09:18:26 +0100

    [pep8] Fix some warnings

--- setup.py
+++ setup.py
@@ -3,22 +3,24 @@
 
 from setuptools import setup
 
-setup(name='ipython-beautifulsoup',
-      version='0.1.1',
-      description='custom rendering of beautifulsoup object in ipython notebook and qtconsole',
-      author='Laurent Peuch',
-      #long_description='',
-      author_email='cortex@worlddomination.be',
-      url='https://github.com/Psycojoker/ipython-beautifulsoup',
-      install_requires=['pygments', 'ipython'],
-      extras_require={
+setup(
+    name='ipython-beautifulsoup',
+    version='0.1.1',
+    description='custom rendering of beautifulsoup object \
+    in ipython notebook and qtconsole',
+    author='Laurent Peuch',
+    #long_description='',
+    author_email='cortex@worlddomination.be',
+    url='https://github.com/Psycojoker/ipython-beautifulsoup',
+    install_requires=['pygments', 'ipython'],
+    extras_require={
         "notebook": ["ipython[notebook]", "pyzmq", "jinja2", "tornado"],
         "qtconsole": ["ipython[qtconsole]"],
         "bs3": ["BeautifulSoup"],
         "bs4": ["BeautifulSoup4"],
-      },
-      py_modules=['soup'],
-      license= 'MIT',
-      scripts=[],
-      keywords='ipython beautifulsoup parsing scraping',
-     )
+    },
+    py_modules=['soup'],
+    license='MIT',
+    scripts=[],
+    keywords='ipython beautifulsoup parsing scraping',
+    )
